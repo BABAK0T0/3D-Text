cca318cc1bfc6930f4bce236b292aa82bbe66ccb
BABAK0T0 <nrandria@student.42.fr>
2021-01-22 13:05:42 +0100

    Add animation and more controls

--- src/script.js
+++ src/script.js
@@ -2,28 +2,44 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
+import gsap, { Circ } from "gsap";
 
 // Debug
 const gui = new dat.GUI();
+gui.hide();
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
 const params = {
   bgColor: "#fcfcfc",
-  //   bgColor: "#152959",
+  //   bgColor: "#222244",
+};
+
+const animation = {
+  introCompleted: false,
+};
+
+const cursor = {
+  x: 0,
+  y: 0,
 };
 
 // Scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(params.bgColor);
-gui.addColor(params, "bgColor").onChange(() => {
-  scene.background.set(params.bgColor);
-});
+gui
+  .addColor(params, "bgColor")
+  .onChange(() => {
+    scene.background.set(params.bgColor);
+  })
+  .name("Background Color");
 
 // Textures
-const textureLoader = new THREE.TextureLoader();
-const matcapTexture = textureLoader.load("/textures/matcaps/9.png");
+// const textureLoader = new THREE.TextureLoader();
+// const matcapTexture = textureLoader.load("/textures/matcaps/9.png");
+
+const meshes = new Array();
 
 // Fonts
 const fontLoader = new THREE.FontLoader();
@@ -36,7 +52,7 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
       height: 0.2,
       curveSegments: 12,
       bevelEnabled: true,
-      bevelThickness: 0.03,
+      bevelThickness: 0.04,
       bevelSize: 0.02,
       bevelOffset: 0,
       bevelSegments: 5,
@@ -44,11 +60,11 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
   );
   textGeometry.center();
 
-  //   const material = new THREE.MeshNormalMaterial();
-  const material = new THREE.MeshMatcapMaterial();
-  material.matcap = matcapTexture;
+  const material = new THREE.MeshNormalMaterial();
+  //   const material = new THREE.MeshMatcapMaterial();
+  //   material.matcap = matcapTexture;
   material.wireframe = false;
-  gui.add(material, "wireframe");
+  gui.add(material, "wireframe").name("Wireframe");
 
   const text = new THREE.Mesh(textGeometry, material);
   const torus = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45);
@@ -57,7 +73,7 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
 
   scene.add(text);
   for (let i = 0; i < 500; i++) {
-    const objectGeometry = i % 2 === 1 ? torus : box;
+    const objectGeometry = i % 2 ? torus : box;
     const mesh = new THREE.Mesh(objectGeometry, material);
 
     // Position
@@ -66,13 +82,13 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
     mesh.position.z = (Math.random() - 0.5) * amplitude;
 
     // Rotation
-    mesh.rotation.x = Math.random() * Math.PI;
-    mesh.rotation.y = Math.random() * Math.PI;
+    mesh.rotation.x = Math.random() * Math.PI * 2;
+    mesh.rotation.y = Math.random() * Math.PI * 2;
 
     // Scale
     const scale = Math.random();
     mesh.scale.set(scale, scale, scale);
-
+    meshes.push(mesh);
     scene.add(mesh);
   }
 });
@@ -97,6 +113,11 @@ window.addEventListener("resize", () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
+window.addEventListener("mousemove", (e) => {
+  cursor.x = e.clientX / sizes.width - 0.5;
+  cursor.y = -(e.clientY / sizes.height - 0.5);
+});
+
 // Camera
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -104,14 +125,18 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.x = 1;
+camera.position.x = 0;
 camera.position.y = 1;
-camera.position.z = 3;
+camera.position.z = 5;
 scene.add(camera);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
+controls.autoRotate = false;
+controls.minDistance = 0.01;
+controls.maxDistance = 20;
+controls.enabled = false;
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -120,18 +145,31 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-// Animate
-const clock = new THREE.Clock();
+// Introduction;
+gsap.from(camera.position, {
+  duration: 3,
+  x: -48,
+  y: 16,
+  z: 50,
+  ease: Circ.easeOut,
+  delay: 1,
+  onComplete: () => {
+    animation.introCompleted = true;
+    controls.enabled = true;
+  },
+});
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-
-  // Update camera
-  camera.position.x = Math.sin(elapsedTime * 0.5);
-  camera.position.y = Math.cos(elapsedTime * 0.5);
-
-  // Update controls
-  controls.update();
+  if (animation.introCompleted) {
+    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 10;
+    camera.position.y = Math.sin(cursor.y * Math.PI * 2) * 10;
+    // Update controls
+    controls.update();
+  }
+  meshes.map((mesh) => {
+    mesh.rotation.x += 0.005;
+    mesh.rotation.y += 0.005;
+  });
 
   // Render
   renderer.render(scene, camera);
